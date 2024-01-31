0aca42edea767d03a05079fd70da31073ab665dc
João Pedro Barbosa <joppemdesu@gmail.com>
2024-01-31 11:35:40 -0400

    All the interface has been created

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,18 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.MusicPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPause = new Siticone.Desktop.UI.WinForms.SiticoneGradientButton();
+            this.btnLoad = new Siticone.Desktop.UI.WinForms.SiticoneGradientButton();
+            this.btnStop = new Siticone.Desktop.UI.WinForms.SiticoneGradientButton();
+            this.btnStart = new Siticone.Desktop.UI.WinForms.SiticoneGradientButton();
+            this.VolumeBar = new Siticone.Desktop.UI.WinForms.SiticoneTrackBar();
+            this.txtVolume = new System.Windows.Forms.Label();
+            this.txtCounter = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtimported = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.MusicPlayer)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // MusicPlayer
@@ -43,23 +54,175 @@
             this.MusicPlayer.TabIndex = 0;
             this.MusicPlayer.Visible = false;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Controls.Add(this.txtCounter);
+            this.panel1.Controls.Add(this.txtVolume);
+            this.panel1.Controls.Add(this.VolumeBar);
+            this.panel1.Controls.Add(this.btnPause);
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Location = new System.Drawing.Point(2, 389);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(798, 69);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnPause
+            // 
+            this.btnPause.BorderRadius = 25;
+            this.btnPause.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnPause.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnPause.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnPause.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnPause.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnPause.FillColor = System.Drawing.Color.Aqua;
+            this.btnPause.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnPause.Font = new System.Drawing.Font("Forte", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPause.ForeColor = System.Drawing.Color.Indigo;
+            this.btnPause.Location = new System.Drawing.Point(320, 13);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(98, 41);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BorderRadius = 25;
+            this.btnLoad.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnLoad.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnLoad.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnLoad.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnLoad.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnLoad.FillColor = System.Drawing.Color.Aqua;
+            this.btnLoad.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnLoad.Font = new System.Drawing.Font("Forte", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.ForeColor = System.Drawing.Color.Indigo;
+            this.btnLoad.Location = new System.Drawing.Point(3, 13);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(101, 41);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load";
+            // 
+            // btnStop
+            // 
+            this.btnStop.BorderRadius = 25;
+            this.btnStop.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnStop.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnStop.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnStop.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnStop.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnStop.FillColor = System.Drawing.Color.Aqua;
+            this.btnStop.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnStop.Font = new System.Drawing.Font("Forte", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.Indigo;
+            this.btnStop.Location = new System.Drawing.Point(217, 13);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(97, 41);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BorderRadius = 25;
+            this.btnStart.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnStart.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnStart.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnStart.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnStart.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnStart.FillColor = System.Drawing.Color.Aqua;
+            this.btnStart.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnStart.Font = new System.Drawing.Font("Forte", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.Indigo;
+            this.btnStart.Location = new System.Drawing.Point(110, 13);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 41);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            // 
+            // VolumeBar
+            // 
+            this.VolumeBar.Location = new System.Drawing.Point(445, 26);
+            this.VolumeBar.Name = "VolumeBar";
+            this.VolumeBar.Size = new System.Drawing.Size(329, 23);
+            this.VolumeBar.TabIndex = 2;
+            this.VolumeBar.ThumbColor = System.Drawing.Color.RoyalBlue;
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.AutoSize = true;
+            this.txtVolume.Font = new System.Drawing.Font("Tw Cen MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVolume.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtVolume.Location = new System.Drawing.Point(442, 13);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(44, 14);
+            this.txtVolume.TabIndex = 2;
+            this.txtVolume.Text = "Volume";
+            // 
+            // txtCounter
+            // 
+            this.txtCounter.AutoSize = true;
+            this.txtCounter.Font = new System.Drawing.Font("Tw Cen MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCounter.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtCounter.Location = new System.Drawing.Point(683, 13);
+            this.txtCounter.Name = "txtCounter";
+            this.txtCounter.Size = new System.Drawing.Size(91, 14);
+            this.txtCounter.TabIndex = 6;
+            this.txtCounter.Text = "Lenght: 0:00/0:00";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(776, 329);
+            this.listBox1.TabIndex = 2;
+            // 
+            // txtimported
+            // 
+            this.txtimported.AutoSize = true;
+            this.txtimported.Font = new System.Drawing.Font("DeVinne Txt BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtimported.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtimported.Location = new System.Drawing.Point(318, 26);
+            this.txtimported.Name = "txtimported";
+            this.txtimported.Size = new System.Drawing.Size(155, 22);
+            this.txtimported.TabIndex = 3;
+            this.txtimported.Text = "Imported Songs:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtimported);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.MusicPlayer);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Haya Music Player";
             ((System.ComponentModel.ISupportInitialize)(this.MusicPlayer)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private AxWMPLib.AxWindowsMediaPlayer MusicPlayer;
+        private System.Windows.Forms.Panel panel1;
+        private Siticone.Desktop.UI.WinForms.SiticoneGradientButton btnPause;
+        private Siticone.Desktop.UI.WinForms.SiticoneGradientButton btnLoad;
+        private Siticone.Desktop.UI.WinForms.SiticoneGradientButton btnStop;
+        private Siticone.Desktop.UI.WinForms.SiticoneGradientButton btnStart;
+        private System.Windows.Forms.Label txtVolume;
+        private Siticone.Desktop.UI.WinForms.SiticoneTrackBar VolumeBar;
+        private System.Windows.Forms.Label txtCounter;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label txtimported;
     }
 }
 
